0596653f0058eb2e2150eef7b0fd1b35a3e2398b
anonymity-developer <bjwon59@gmail.com>
2025-04-24 17:35:42 +0900

    rebase

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -38,7 +38,7 @@ team_t team = {
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7) //0000 01111
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
